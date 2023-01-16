8efe0f77a02b4fcd46922f1e5ca1095f61ea6c12
araujoleonardo <araujoleonardo.ti#gmail.com>
2023-01-16 19:42:09 -0400

    Estruturação do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,45 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <RouterView />
+    <Footer />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+    import Navbar from './components/Navbar.vue';
+    import Footer from './components/Footer.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+    export default {
+        components: {
+            Navbar,
+            Footer
+        },
+        data() {
+            return {
+                logo_src: "/img/logo.png",
+                app_name: "Make Your Burger"
+            }
+        }
+    }
+</script>
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+<style>
+    * {
+        font-family: Helvetica, sans-serif;
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+    .main-container {
+        margin: 50px;
+        min-height: 250px;
+    }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+    h1 {
+        text-align: center;
+        font-size: 42px;
+        margin-bottom: 30px;
+        color: #222;
+    }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="nav">
+        <router-link to="/" id="logo-url">
+            <img :src="logo" :alt="alt" id="logo">
+        </router-link>    
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/pedidos">Pedidos</RouterLink>
+    </div>    
+</template>
+
+<script>
+    export default {
+        name: "Navbar",
+        props: ["logo", "alt"]
+    }
+</script>
+
+<style scoped>
+    #nav {
+        background-color: #222;
+        border-bottom: 4px solid #111;
+        padding: 15px 50px;
+        margin: 0;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+
+    #nav #logo-url {
+        margin: auto;
+        margin-left: 0;
+    }
+
+    #logo {
+        width: 40px;
+        height: 40px;
+    }
+
+    #nav a {
+        color: rgb(250, 103, 50);
+        text-decoration: none;
+        margin: 12px;
+        transition: .5s;
+    }
+
+    #nav a:hover {
+        color: #fff;
+    }
+</style>
