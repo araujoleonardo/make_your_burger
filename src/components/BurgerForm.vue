3f89f78f5664ed84f87c8df0f36e9d62b6e14790
araujoleonardo <araujoleonardo.ti@gmail.com>
2023-01-22 15:32:26 -0400

    Finalização do form de pedidos

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <p>Componente de mensagem</p>
+        <Message :msg="msg" v-show="msg"/>
         <div>
-            <form id="burger-form">
+            <form id="burger-form" @submit="createBurger">
                 <div class="input-container">
                     <label for="nome">Nome do cliente: </label>
                     <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome...">
@@ -37,6 +37,8 @@
 </template>
 
 <script>
+import Message from './Message.vue';
+
 export default {
     name: "BurgerForm",
     data() {
@@ -48,23 +50,60 @@ export default {
            pao: null,
            carne: null,
            opcionais: [],
-           status: "Solicitado",
            msg: null 
         }
     },
     methods: {
         async getIngredientes() {
 
-            const req = await fetch("http://localhost:3000/ingredientes");
-            const data = await req.json();
+            const req = await fetch("http://localhost:3000/ingredientes")
+            const data = await req.json()
 
             this.paes = data.paes;
             this.carnes = data.carnes;
             this.opcionaisdata = data.opcionais;
+        },
+        async createBurger(e) {
+
+            e.preventDefault()
+
+            const data = {
+                nome: this.nome,
+                carne: this.carne,
+                pao: this.pao,
+                opcionais: Array.from(this.opcionais),
+                status: "Solicitado"
+            }
+
+            //Enviar dados como texto para servidor
+            const dataJson = JSON.stringify(data)
+
+            const req = await fetch("http://localhost:3000/burgers", {
+                method: "POST",
+                headers: { "Content-Type" : "application/json" },
+                body: dataJson
+            });
+
+            const res = await req.json()
+
+            //Mensagem do sistema
+            this.msg = `Pedido Nº ${res.id} realizado com sucesso!`;
+
+            //Limpar mensagem
+            setTimeout(() => this.msg = "", 3000);
+
+            //Limpar campos
+            this.nome = "";
+            this.carne = "";
+            this.pao = "";
+            this.opcionais = "";
         }
     },
     mounted() {
         this.getIngredientes()
+    },
+    components: {
+        Message
     }
 }
 </script>
